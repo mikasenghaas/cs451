7177dc5a668cf9489878ddd7f56d5a3e549fbc5e
Mika Senghaas <mail@mikasenghaas.de>
2024-11-05 20:00:20 +0100

    Adjust example to be run from cpp_template

--- cpp_template/example.sh
+++ cpp_template/example.sh
@@ -4,20 +4,19 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
 
 # Build the application
-log_file="example/output/example.stdout"
+log_file="../example/output/example.stdout"
 bash $DIR/build.sh >> $log_file
 
 # Remove previous outputs
-rm -f example/output/*.output 2>/dev/null
-rm -f example/output/*.log 2>/dev/null
-rm -f example/output/log 2>/dev/null
+rm -f ../example/output/*.output 2>/dev/null
+rm -f ../example/output/*.stdout 2>/dev/null
 
 # Read hosts
-num_processes=$(cat example/hosts | wc -l)
+num_processes=$(cat ../example/hosts | wc -l)
 echo "Number of processes: $num_processes" >> $log_file 
 
 # Read config
-receiver_id=$(cat example/configs/perfect-links.config | cut -d ' ' -f2)
+receiver_id=$(cat ../example/configs/perfect-links.config | cut -d ' ' -f2)
 echo "Receiver ID: $receiver_id" >> $log_file 
 
 # Store PIDs for cleanup
@@ -25,7 +24,7 @@ pids=()
 
 # Start processes
 for i in $(seq $num_processes -1 1); do
-    $DIR/bin/da_proc --id $i --hosts example/hosts --output example/output/proc$(printf "%02d" $i).output example/configs/perfect-links.config > example/output/proc$(printf "%02d" $i).stdout &
+    $DIR/bin/da_proc --id $i --hosts ../example/hosts --output ../example/output/proc$(printf "%02d" $i).output ../example/configs/perfect-links.config > ../example/output/proc$(printf "%02d" $i).stdout &
     pids+=($!)
 done
 echo "PIDs: ${pids[@]}" >> $log_file
@@ -43,4 +42,4 @@ done
 echo "Done" >> $log_file
 
 # Validate the output files
-python tools/validate/perfect_link.py example/configs/perfect-links.config example/hosts example/output
+python ../tools/validate/perfect_link.py ../example/configs/perfect-links.config ../example/hosts ../example/output
