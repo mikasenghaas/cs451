6e257a0f8772799cf9a7d60f329d5d6f22741816
Mika Senghaas <mail@mikasenghaas.de>
2024-11-21 18:33:57 +0100

    Implement FIFO correctness test

--- tools/correctness.py
+++ tools/correctness.py
@@ -21,16 +21,45 @@ def check_receiver(outputs: List[str], num_hosts: int, id: int, n: int):
                 received[sender_id] += 1
     return received
 
-def check_beb(outputs: List[str], num_hosts: int, message_count: int):
-    hosts = list(range(1, num_hosts+1))
+def check_beb(outputs: List[str], num_hosts: int, message_count: int, host_id: int):
+    outputs_set = set(outputs)
     messages = list(range(1, message_count+1))
     broadcast_messages = [f"b {m}" for m in messages]
-    deliver_messages = [f"d {h} {m}" for h in hosts for m in messages]
-    # Check if all messages were broadcasted and delivered
-    assert set(broadcast_messages).issubset(set(outputs)), "Not all broadcast messages were sent"
-    assert set(deliver_messages).issubset(set(outputs)), "Not all deliver messages were sent"
-    # Check no duplication
-    assert len(deliver_messages) == len(set(deliver_messages)), "Duplicate messages delivered"
+    deliver_messages = [f"d {h} {m}" for h in range(1, num_hosts+1) for m in messages]
+    
+    # Check all broadcasts were sent
+    for bm in broadcast_messages:
+        assert bm in outputs_set, f"[Host {host_id}] Broadcast message {bm} was not sent"
+
+    # Check all delivers were received
+    for dm in deliver_messages:
+        assert dm in outputs_set, f"[Host {host_id}] Message {dm} was not delivered"
+
+    assert len(deliver_messages) == len(set(deliver_messages)), f"[Host {host_id}] Duplicate messages delivered"
+
+    return True
+
+def check_fifo(outputs: List[str], num_hosts: int, host_id: int):
+    # Check broadcast order
+    broadcasted = [msg for msg in outputs if msg.startswith("b")]
+    last_seq = 0
+    for msg in broadcasted:
+        seq = int(msg.split()[-1])
+        assert seq == last_seq + 1, f"[{host_id}] Broadcast message {seq} before {last_seq+1}"
+        last_seq = seq
+
+    # Check delivery order per host
+    hosts = list(range(1, num_hosts+1))
+    for host in hosts:
+        delivered = [msg for msg in outputs if int(msg.split()[1]) == host and msg.startswith("d")]
+        
+        last_seq = 0
+        for msg in delivered:
+            seq = int(msg.split()[-1])
+            assert seq == last_seq + 1, f"[{host_id}] Got message {seq} before {last_seq+1} from host {host}"
+            last_seq = seq
+
+    return True
 
 def main(args):
     config_file = args.config
@@ -92,13 +121,14 @@ def main(args):
         assert all(os.path.exists(os.path.join(log_dir, f"proc{i:02d}.output")) for i in range(1, num_hosts + 1)), msg
 
         # Check BEB validation
-        for process_id in range(1, num_hosts + 1):
-            with open(os.path.join(log_dir, f"proc{process_id:02d}.output"), "r") as f:
+        for host_id in range(1, num_hosts + 1):
+            with open(os.path.join(log_dir, f"proc{host_id:02d}.output"), "r") as f:
                 outputs = list(map(str.strip, f.readlines()))
-                check_beb(outputs, num_hosts, message_count)
-
-
+                check_beb(outputs, num_hosts, message_count, host_id)
+                check_fifo(outputs, num_hosts, host_id)
+        
         print("BEB validation passed ✅")
+        print("FIFO validation passed ✅")
     else:
         raise ValueError(f"Invalid command: {args.command}")
 
